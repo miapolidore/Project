1fa2e8493ce87f453e8cdfae24a250631f7b3cf9
miapolidore <156038306+miapolidore@users.noreply.github.com>
2024-04-15 20:20:02 -0500

    IT WORKS

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import mysql.connector # I don't understand why this is happening, "Defaulting to user installation because normal site-packages is not writeable...?"
+import mysql.connector as mysql # I don't understand why this is happening, "Defaulting to user installation because normal site-packages is not writeable...?"
+# FIXED IT!! By creating a virtual environment I was able to identify that I has a $PATH conflict.
 
 # Going to attempt & restore previous progress while handling with the bug later.
 
-connection = mysql.connector.connect(host = 'localHost', user  = 'root', password = 'mySqlPassword', database = 'bank')
+connection = mysql.connect(host = 'localHost', user  = 'root', password = 'mySqlPassword', database = 'bank')
 
 '''
 cursor = connection.cursor()
